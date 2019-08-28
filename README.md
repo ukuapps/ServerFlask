de85258976a82deae563373e589c462dd5a76423
joan <joan.silva.1986@gmail.com>
2019-08-28 00:43:33 -0500

    Encontre un ejemplo completo donde usan flask restful

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ServerFlask
+
+#no olvidar verificar el entorno con el comando freeze para verificar que los entornos sean los mismo.
--- main.py
+++ main.py
@@ -1,18 +1,62 @@
 from flask import Flask
-import json
+from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
-PORT = 8000
-DEBUG = True
+api = Api(app)
 
-@app.errorhandler(404)
-def not_found(error):
-    return "Not found."
+TODOS = {
+    'todo1': {'task': 'build an API'},
+    'todo2': {'task': '?????'},
+    'todo3': {'task': 'profit!'},
+}
 
-@app.route('/', methods=['GET'])
-def index():
-    data = {"api":"hola mundo"}
-    return json.dumps(data)
+
+def abort_if_todo_doesnt_exist(todo_id):
+    if todo_id not in TODOS:
+        abort(404, message="Todo {} doesn't exist".format(todo_id))
+
+parser = reqparse.RequestParser()
+parser.add_argument('task')
+
+
+# Todo
+# shows a single todo item and lets you delete a todo item
+class Todo(Resource):
+    def get(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        return TODOS[todo_id]
+
+    def delete(self, todo_id):
+        abort_if_todo_doesnt_exist(todo_id)
+        del TODOS[todo_id]
+        return '', 204
+
+    def put(self, todo_id):
+        args = parser.parse_args()
+        task = {'task': args['task']}
+        TODOS[todo_id] = task
+        return task, 201
+
+
+# TodoList
+# shows a list of all todos, and lets you POST to add new tasks
+class TodoList(Resource):
+    def get(self):
+        return TODOS
+
+    def post(self):
+        args = parser.parse_args()
+        todo_id = int(max(TODOS.keys()).lstrip('todo')) + 1
+        todo_id = 'todo%i' % todo_id
+        TODOS[todo_id] = {'task': args['task']}
+        return TODOS[todo_id], 201
+
+##
+## Actually setup the Api resource routing here
+##
+api.add_resource(TodoList, '/todos')
+api.add_resource(Todo, '/todos/<todo_id>')
+
 
 if __name__ == '__main__':
-    app.run(port = PORT, debug = DEBUG)
+    app.run(debug=True)
